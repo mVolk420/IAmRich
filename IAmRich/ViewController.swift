8ff7c7151be3a9374031cb84fb0485a4f3f688f9
mvolk420 <mvolk587@gmail.com>
2025-04-28 14:43:35 +0200

    init IAmRich IOS App

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Michael Volk on 26.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IAmRichTests/IAmRichTests.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichTests.swift
+//  IAmRichTests
+//
+//  Created by Michael Volk on 26.04.25.
+//
+
+import Testing
+@testable import IAmRich
+
+struct IAmRichTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
